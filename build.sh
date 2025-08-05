9598b839808cb8e0e3519db611851cb08fc4cb1e
Charvi Mishra <09charvimishra@gmail.com>
2025-08-05 15:09:27 +0530

    deploy3

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-# Ensure pip and core build tools are available before installation
-pip install --upgrade pip setuptools wheel setuptools_scm
-
-# Then install project requirements
+# Simple build script for Render
+pip install --upgrade pip
 pip install -r requirements.txt
-#!/usr/bin/env bash
-
-
