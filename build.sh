71a9b9660f2dee505d2e4e4ed79018873d5f30dc
Charvi Mishra <09charvimishra@gmail.com>
2025-08-05 14:33:16 +0530

    changes

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Ensure pip and core build tools are available before installation
-pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel setuptools_scm
 
 # Then install project requirements
 pip install -r requirements.txt
+#!/usr/bin/env bash
+
+
